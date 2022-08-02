9a681ce61d0ccce79faddd4efb121679403dcbd4
Adrk51 <durakahmet049@gmail.com>
2022-08-02 17:09:13 +0200

    HamcrestMatchersIntro

--- src/test/java/com/cybertek/Day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/Day5/HamcrestMatchersIntro.java
@@ -1,10 +1,17 @@
 package com.cybertek.Day5;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+
 public class HamcrestMatchersIntro {
+    @DisplayName("Matcher Demo")
     @Test
     public void test1(){
-        
+        assertThat(5+5,is(10));
     }
 }

930948cdbebec9095a5b4b82e01dfc37e9551579
Adrk51 <durakahmet049@gmail.com>
2022-08-02 17:38:41 +0200

    ii_HamcrestMatcherApiTest is added

--- src/test/java/com/cybertek/Day5/i_HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/Day5/i_HamcrestMatchersIntro.java
@@ -8,9 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 
-public class HamcrestMatchersIntro {
+public class i_HamcrestMatchersIntro {
     @DisplayName("Matcher Demo")
     @Test
     public void test1() {
---
+++ src/test/java/com/cybertek/Day5/ii_HamcrestMatcherApiTest.java
@@ -0,0 +1,16 @@
+package com.cybertek.Day5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ii_HamcrestMatcherApiTest {
+    @DisplayName("OneSpartan with Hamcrest and chaining")
+    @Test
+    public void test1(){
+
+    }
+}

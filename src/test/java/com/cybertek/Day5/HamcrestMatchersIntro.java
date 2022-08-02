adc38025cb2505303b4d22e72f1f705fd051e909
Adrk51 <durakahmet049@gmail.com>
2022-08-02 17:14:43 +0200

    HamcrestMatchersIntro test1 method is added

--- src/test/java/com/cybertek/Day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/Day5/HamcrestMatchersIntro.java
@@ -13,5 +13,22 @@ public class HamcrestMatchersIntro {
     @Test
     public void test1(){
         assertThat(5+5,is(10));
+        assertThat(5+5,equalTo(10));
+        //matchers have 2 overloaded version
+        //first that accept actual value
+        //second taht accept another matchers
+        //below examples is method is accepting another matchers equal to make it readable
+        assertThat(5+5,is(equalTo(10)));
+
+        assertThat(5+5, not(9));
+        assertThat(5+5, is(not(9)));
+        assertThat(5+5, is(not(equalTo(10))));
+
+        //number comparison
+        //greaterThan()
+        //greaterThanOrEqualTo()
+        //lessThan()
+        //lessThanOrEqualTo()
+        assertThat(5+5,is(greaterThan(9)));
     }
 }

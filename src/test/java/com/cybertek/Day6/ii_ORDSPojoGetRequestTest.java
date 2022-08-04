f6382c7af2250cf34d90b245ca96298045262a0b
Adrk51 <durakahmet049@gmail.com>
2022-08-04 12:31:40 +0200

    @JsonProperty('variableName')

--- src/test/java/com/cybertek/Day6/ii_ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/Day6/ii_ORDSPojoGetRequestTest.java
@@ -1,13 +1,14 @@
 package com.cybertek.Day6;
 
+import com.cybertek.pojo.Employee;
 import com.cybertek.pojo.Region;
 import com.cybertek.utilities.HRTestBase;
 import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 public class ii_ORDSPojoGetRequestTest extends HRTestBase {
     @Test
@@ -17,8 +18,19 @@ public class ii_ORDSPojoGetRequestTest extends HRTestBase {
         Region region1 = jsonPath.getObject("items[0]", Region.class);
         System.out.println("region1 = " + region1);
 
-        System.out.println("getRegion_id() = " + region1.getRegion_id());
-        System.out.println("getRegion_name() = " + region1.getRegion_name());
-        System.out.println("getHref() = " + region1.getLinks().get(0).getHref());
+        System.out.println("getRegion_id() = " + region1.getRegionId());
+        System.out.println("getRegion_name() = " + region1.getRegionName());
+        System.out.println("getHref() = " + region1.getLinkList().get(0).getHref());
+    }
+
+    @DisplayName("GET request to /employees and only get couple of values as a Pojo class")
+    @Test
+    public void employeeGet(){
+
+        Employee employee1 = get("/employees").then().statusCode(200)
+                .extract().jsonPath().getObject("items[0]", Employee.class);
+
+        System.out.println(employee1);
+
     }
 }
---
+++ src/test/java/com/cybertek/pojo/Regions.java
@@ -0,0 +1,17 @@
+package com.cybertek.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties
+public class Regions {
+    private List<Region> regionList;
+    private int count;
+}
